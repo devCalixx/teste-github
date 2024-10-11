c30b26774efa4c6756a4e73377451e40f5920164
devCalixx <julia.calixto@aluno.ifsp.edu.br>
2024-10-10 21:18:07 -0300

    sprite e script
    Tá, eu mudei bastante coisa. peguei os sprites do personagem padrão peladinho (já que a gente não sabe exatamente ainda qual sprite vai usar) e fiz os movimentos de direita e esquerda dele, dele andando.
    
    também fiz os movimentos de cima pra quando ele for pro metrô, mas na primeira fase fica MUITO ZUADO (parece o modo criativo do minecraft mas pior).
    
    aí eu tava querendo ver como fazia pra falar tipo: se o personagem estiver nesse mundo aqui, ele tem acesso a essas funções. se não, não.
    
    deixem em comentário essas funções de ir pra cima e pra baixo. acredito que elas funcionem se tiver realmente um cenário onde dê pra ir pra cima e pra baixo.
    
    a parte do tileset tá meio bagunçada, mas vou ver se dou uma ajeitada. tá essa zona porque os personagens que eu salvei tavam com 1024px e precisava só de 32px (se alguém ver antes e quiser já ajeitar na pasta, beleza).
    
    também coloquei algumas outras funções, como sentar, levantar, sentar de lado, etc, mas ainda nao chamei no script porque precisamos do outro cenário, dos banquinhos para colidir e etc.
    
    já dá pra fazer os npcs nesse estilinho ou acham melhor fazer naquele outro?

--- script/personagem.gd
+++ script/personagem.gd
@@ -5,22 +5,47 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+
+	if Input.is_action_just_pressed("pular") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x := Input.get_axis("mover_esquerda", "mover_direita")
+	
+	if direction_x > 0:
+		animated_sprite.flip_h = false
+	elif direction_x < 0:
+		animated_sprite.flip_h = true
+	
+	if direction_x == 0:
+		animated_sprite.play("parado")
+	else:
+		animated_sprite.play("andar-horizontal")
+	
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#queria fazer um negócio tipo: se o personagem tá no mundo do metrô, ele pode ter acesso a essas funções de ir pra cima e para baixo. se não, não."	
+		
+	#var direction_y := Input.get_axis("mover_cima", "mover_baixo")
+	
+	#if direction_y > 0:
+		#animated_sprite.play("andar-baixo")
+	#elif direction_y < 0:
+		#animated_sprite.play("andar-cima")
+	#elif direction_y == 0:
+		#animated_sprite.play("parado")
+
+	#if direction_y:
+		#velocity.y = direction_y * SPEED
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
